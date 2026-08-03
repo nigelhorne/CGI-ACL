aebc18ebb0ac60d5f27f105006f8fee431dc9690
Nigel Horne <njh@bandsman.co.uk>
2026-08-02 20:58:24 -0400

    Release 0.09

--- lib/CGI/ACL.pm
+++ lib/CGI/ACL.pm
@@ -79,11 +79,11 @@ CGI::ACL - Decide whether to allow a client to run a CGI script
 
 =head1 VERSION
 
-Version 0.08
+Version 0.09
 
 =cut
 
-our $VERSION = '0.08';
+our $VERSION = '0.09';
 
 =head1 SYNOPSIS
 
